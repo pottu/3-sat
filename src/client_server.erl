b7aac1cf6c6c371cc2e066b3dae07cd48016ba2b
Pontus Ernstedt <pontus_ponta@hotmail.com>
2021-01-11 15:38:52 +0100

    Initial client server

--- src/client_server.erl
+++ src/client_server.erl
@@ -1,29 +1,25 @@
--module(sat_server).
+-module(client_server).
 
 -behaviour(gen_server).
 
 -import(solver, [solve/1]).
 
 %% API
--export([start_link/1, start_link/0, get_count/0, stop/0, run/0]).
+-export([start_link/1, stop/0, run/0]).
 
 %% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+-export([init/1, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
 -define(SERVER, ?MODULE).
--define(DEFAULT_PORT, 3547).
--record(state, {port, lsock, request_count = 0}).
+-record(state, {socket, result = none, solver = none}).
 
 run() ->
     io:format("Server module~n").
 
 % API functions
-start_link(Port) ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [Port], []).
-
-start_link() ->
-    start_link(?DEFAULT_PORT).
+start_link(Socket) ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [Socket], []).
 
 get_count() ->
     gen_server:call(?SERVER, get_count).
@@ -31,33 +27,54 @@ get_count() ->
 stop() ->
     gen_server:cast(?SERVER, stop).
 
-% gen_server callbacks
-init([Port]) ->
-    {ok, LSock} = gen_tcp:listen(Port, [{active, true}, {reuseaddr, true}]),
-    {ok, Sock} = gen_tcp:accept(LSock),
-    gen_tcp:send(Sock, io_lib:fwrite("~w~n", [ready])),
-    {ok, #state{port = Port, lsock = LSock}, 0}.
-
-handle_call(get_count, _From, State) ->
-    {reply, {ok, State#state.request_count}, State}.
+% ---- gen_server callbacks --------------------------------
+init([Socket]) ->
+    gen_tcp:send(Socket, io_lib:fwrite("~w~n", [ready])),
+    {ok, #state{socket = Socket}, 0}.
 
 handle_cast(stop, State) ->
     {stop, normal, State}.
 
+% Handle client abort message.
+handle_info({tcp, Socket, "abort"}, State = #state{solver = Solver}) ->
+  case Solver of
+    none -> ok;
+    Pid  -> exit(Pid, kill)
+  end,
+  gen_tcp:send(Socket, io_lib:fwrite("~w~n", [aborted])),
+  {noreply, State#state{solver = none}};
+
+% Handle client instance.
 handle_info({tcp, Socket, String}, State) ->
     {ok, Ts, _} = erl_scan:string(String ++ "."),
     {ok, Term} = erl_parse:parse_term(Ts),
-    Result = solve(Term),
-    gen_tcp:send(Socket, io_lib:fwrite("Res: ~w~n", [Result])),
-    RequestCount = State#state.request_count,
-    {noreply, State#state{request_count = RequestCount + 1}};
+    % TODO: Check if Term is an instance.
+    Instance = Term,
+    Pid = spawn_link(solver, solve, [self(), Instance]),
+    erlang:send_after(10000, self(), pulse),
+    {noreply, State#state{solver = Pid}};
 
-handle_info({tcp_closed, _Socket}, #state{lsock = LSock} = State) ->
-    {ok, _Sock} = gen_tcp:accept(LSock),
+% Handle solver message.
+handle_info({solution, Result}, State = #state{socket = Socket}) ->
+  gen_tcp:send(Socket, io_lib:fwrite("~w~n", [Result])),
+  {noreply, State#state{solver = none}};
+
+% Handle closed client connection.
+handle_info({tcp_closed, _Socket}, State) ->
+    % TODO: stop() here?
     {noreply, State};
 
-handle_info(timeout, #state{lsock = LSock} = State) ->
-    {ok, _Sock} = gen_tcp:accept(LSock),
+% Handle pulse.
+handle_info(pulse, State = #state{socket = Socket, result = Result, solver = Solver}) ->
+  case Solver of
+    none -> gen_tcp:send(Socket, io_lib:fwrite("~w~n", [Result]));
+    _Pid -> gen_tcp:send(Socket, io_lib:fwrite("~w~n", [trying]))
+  end,
+  erlang:send_after(10000, self(), pulse),
+  {noreply, State};
+
+handle_info(timeout, State) ->
+    % TODO: stop() here?
     {noreply, State}.
 
 terminate(_Reason, _State) ->
