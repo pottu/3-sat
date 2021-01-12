3858db7f69918eb3adb7aeee9d640b00a83a48c7
Pontus Ernstedt <pontus_ponta@hotmail.com>
2021-01-12 15:23:43 +0100

    Client server impl

--- src/client_server.erl
+++ src/client_server.erl
@@ -5,93 +5,96 @@
 -import(solver, [is_instance/1, solve/1]).
 
 %% API
--export([start_link/0, start_link/1, stop/0, run/0]).
+-export([start_link/0]).
 
 %% gen_server callbacks
--export([init/1, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+-export([init/1, handle_info/2, terminate/2, handle_call/3, handle_cast/2]).
 
--define(SERVER, ?MODULE).
--record(state, {socket, result = no_result, solver = no_solver}).
+-record(state, {socket = no_socket, msg = no_msg, solver = no_solver, timeout = no_timer}).
 
-run() ->
-    io:format("Server module~n").
+-define(CLIENT_TIMEOUT, 60000).
+-define(PULSE_INTERVAL, 10000).
 
 % API functions
-start_link(Socket) ->
-    gen_server:start_link(?MODULE, [Socket], []).
-
 start_link() ->
   gen_server:start_link(?MODULE, [], []).
 
-stop() ->
-    gen_server:cast(?SERVER, stop).
-
 % ---- gen_server callbacks --------------------------------
-%init([Socket]) ->
-%    gen_tcp:send(Socket, io_lib:fwrite("~w~n", [ready])),
-%    {ok, #state{socket = Socket}}.
-
 init([]) ->
-    {ok, #state{socket = no_socket}}.
+  {ok, #state{}}.
 
-handle_cast(stop, State) ->
-    {stop, normal, State}.
-
+% Handle client socket receival.
 handle_info({socket, Socket}, State) ->
-  gen_tcp:send(Socket, io_lib:fwrite("~w~n", [ready])),
-  erlang:send_after(10000, self(), pulse),
-  {noreply, State#state{socket = Socket}};
-
+  Msg = io_lib:fwrite("ready~n", []),
+  gen_tcp:send(Socket, Msg),
+  erlang:send_after(?PULSE_INTERVAL, self(), pulse),
+  TRef = erlang:send_after(?CLIENT_TIMEOUT, self(), client_timeout),
+  {noreply, State#state{socket = Socket, msg = Msg, timeout = TRef}};
 
 % Handle client message.
-handle_info({tcp, Socket, String}, State = #state{solver = Solver}) ->
+handle_info({tcp, Socket, String}, State = #state{solver = Solver, timeout = Timeout}) ->
+  gen_tcp:send(Socket, io_lib:print(String)),
+  try
     {ok, Ts, _} = erl_scan:string(String ++ "."),
     {ok, Term} = erl_parse:parse_term(Ts),
-    % TODO: Check if Term is an instance.
-    case {Term, Solver, is_instance(Term)} of
-      {abort, Pid, _} when is_pid(Pid) -> 
+    case {Term, Solver} of
+      {abort, Pid} when is_pid(Pid) -> 
         exit(Pid, kill),
-        gen_tcp:send(Socket, io_lib:fwrite("aborted~n", [])),
-        {noreply, State#state{solver = no_solver, result = aborted}};
-      {Instance, no_solver, true} -> 
-        Pid = spawn(solver, solve, [self(), Instance]),
-        gen_tcp:send(Socket, io_lib:fwrite("trying~n", [])),
-        {noreply, State#state{solver = Pid}};
-      _Otherwise ->
-        gen_tcp:send(Socket, io_lib:fwrite("ignored~n", [])),
-        {noreply, State}
-    end;
+        Msg = io_lib:fwrite("aborted~n", []),
+        gen_tcp:send(Socket, Msg),
+        TRef = erlang:send_after(?CLIENT_TIMEOUT, self(), client_timeout),
+        {noreply, State#state{solver = no_solver, msg = Msg, timeout = TRef}};
+      {_Term, no_solver} -> 
+        case is_instance(Term) of
+          true ->
+            Pid = spawn(solver, solve, [self(), Term]),
+            Msg = io_lib:fwrite("trying~n", []),
+            gen_tcp:send(Socket, Msg),
+            erlang:cancel_timer(Timeout),
+            {noreply, State#state{solver = Pid, msg = Msg, timeout = no_timer}}
+        end
+    end
+  catch
+    _:_ ->
+      Msg2 = io_lib:fwrite("ignored~n", []),
+      gen_tcp:send(Socket, Msg2),
+      {noreply, State#state{msg = Msg2}}
+  end;
 
 % Handle solver message.
 handle_info({solution, Result}, State = #state{socket = Socket}) ->
-  gen_tcp:send(Socket, io_lib:fwrite("~w~n", [Result])),
-  {noreply, State#state{solver = no_solver, result = Result}};
+  Msg = io_lib:fwrite("~w~n", [Result]),
+  gen_tcp:send(Socket, Msg),
+  TRef = erlang:send_after(?CLIENT_TIMEOUT, self(), client_timeout),
+  {noreply, State#state{solver = no_solver, msg = Msg, timeout = TRef}};
 
 % Handle closed client connection.
 handle_info({tcp_closed, _Socket}, State) ->
-    % TODO: stop() here
-    {noreply, State};
+  {stop, tcp_closed, State};
+
+% Handle closed client connection.
+handle_info({tcp_error, _Socket, _Reason}, State) ->
+  {stop, tcp_error, State};
 
 % Handle pulse.
-handle_info(pulse, State = #state{socket = Socket, result = Result, solver = Solver}) ->
-  case Solver of
-    no_solver -> 
-      case Result of
-        no_result -> ok;
-        _Result   -> gen_tcp:send(Socket, io_lib:fwrite("~w~n", [Result]))
-      end;
-    _Pid -> gen_tcp:send(Socket, io_lib:fwrite("trying~n", []))
-  end,
-  erlang:send_after(10000, self(), pulse),
+handle_info(pulse, State = #state{socket = Socket, msg = Msg}) ->
+  gen_tcp:send(Socket, Msg),
+  erlang:send_after(?PULSE_INTERVAL, self(), pulse),
   {noreply, State};
 
-handle_info(timeout, State) ->
-    % TODO: stop() here?
-    {noreply, State}.
+% Handle timeout.
+handle_info(client_timeout, State = #state{socket = Socket}) ->
+  gen_tcp:send(Socket, io_lib:fwrite("timeout~n", [])),
+  {stop, client_timeout, State}.
 
-terminate(_Reason, _State) ->
-    ok.
+terminate(_Reason, #state{solver = Solver}) ->
+  case is_pid(Solver) of
+    true -> exit(Solver, kill);
+    _    -> ok
+  end.
 
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+handle_call(_Request, _From, State) ->
+  {noreply, State}.
+
+handle_cast(_Request, State) ->
+  {noreply, State}.
