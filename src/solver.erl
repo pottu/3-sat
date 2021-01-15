5c4ebb1a770701166974548ebacb196825ab9bf3
Jonas Nygren <jonas_n_92@hotmail.com>
2021-01-15 17:27:13 +0100

    Added all tests to solver.erl

--- src/solver.erl
+++ src/solver.erl
@@ -2,6 +2,9 @@
 -compile(export_all).
 -export([is_instance/1, solve/1]).
 
+-include_lib("proper/include/proper.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
 -type clause() :: {integer(), integer(), integer()}.
 -type variable() :: pos_integer().
 -type instance() :: [clause()].
@@ -137,3 +140,150 @@ truth_value(T, A) ->
     T > 0 -> Val;
     T < 0 -> not Val
   end.
+
+%%% --------------------------------------------------------
+%%% ---- PROPER TESTS --------------------------------------
+%%% --------------------------------------------------------
+
+% ---- Generators ------------------------------------------
+clausify([T1,T2,T3|Rest]) -> [{T1,T2,T3} | clausify(Rest)];
+clausify(_) -> [].
+
+gen_instance() ->
+  ?LET(Terms, list(union([pos_integer(), neg_integer()])), clausify(Terms)).
+
+gen_unsat_instance() ->
+  ?LET(Inst, gen_instance(),
+    begin
+      InstWithBadClauses = [{1,1,1},{-1,-1,-1} | Inst],
+      Shuffled = lists:sort([{rand:uniform(), C} || C <- InstWithBadClauses]),
+      [C || {_, C} <- Shuffled]
+    end
+  ).
+
+gen_assignment(I) ->
+    Vars = lists:usort(lists:flatten([[abs(T1),abs(T2),abs(T3)] || {T1,T2,T3} <- I])),
+    NrOfVars = length(Vars),
+    ?LET(Bools, vector(NrOfVars, boolean()),
+      orddict:from_list(lists:zip(Vars, Bools))).
+
+% ---- Checker ---------------------------------------------
+prop_checker_fail_unsat_instance() ->
+  ?FORALL(I, gen_unsat_instance(),
+    ?FORALL(A, gen_assignment(I),
+      not solver:check(I,A)
+    )
+  ).
+
+prop_instance_is_instance() ->
+  ?FORALL(I, gen_instance(), is_instance(I)).
+
+% ---- Solver ---------------------------------------------
+prop_solver_solve_sat_instance() ->
+  ?FORALL(I, gen_instance(),
+      is_sat(solver:solve(I))
+  ).
+
+%% prop_solver_fail_unsat_instance() ->
+%%   ?FORALL(I, gen_unsat_instance(),
+%%       not is_sat(solver:solve(I))
+%%   ).
+
+% ---- Helper functions ---------------------------------------------
+prop_to_bool_pos() ->
+    ?FORALL(I, pos_integer(), to_bool(I)).
+
+prop_to_bool_neg() ->
+    ?FORALL(I, neg_integer(), to_bool(I)).
+
+% ---- Test helpers ---------------------------------------------
+is_sat({sat, _}) ->
+    true;
+
+is_sat(_) ->
+    false.
+
+%%% --------------------------------------------------------
+%%% ---- EUNIT TESTS ---------------------------------------
+%%% --------------------------------------------------------
+
+is_instance_test_() -> [
+    ?_assertEqual(false, is_instance(atom)),
+    ?_assertEqual(false, is_instance(1)),
+    ?_assertEqual(false, is_instance("string")),
+    ?_assertEqual(false, is_instance([{1,2}])),
+    ?_assertEqual(false, is_instance([{1,2,atom}])),
+    ?_assertEqual(false, is_instance([{1,2,0}])),
+    ?_assertEqual(true, is_instance([])),
+    ?_assertEqual(true, is_instance([{1,2,3},{-1,-2,-3}]))
+].
+    
+solve_test_() -> 
+    [
+     ?_assertEqual({sat, []}, solve([])),
+     ?_assertEqual({sat, [true, false, false]}, solve([{1, 2, 3}])),
+     ?_assertEqual({sat, [true, false, false]}, solve([{1,2,3},{-1,-2,-3}])),
+     ?_assertEqual(unsat, solve([{1,1,1},{-1,-1,-1}])),
+     ?_assertEqual({sat, [false]}, solve([{1, 1, -1}]))
+    ].
+
+bit_test_() ->
+    [
+     ?_assertEqual(1, bit(0, 3)),
+     ?_assertEqual(1, bit(1, 3)),
+     ?_assertEqual(0, bit(2, 3))
+    ].
+
+to_bool_test_() ->
+    [
+     ?_assertEqual(true, to_bool(1)),
+     ?_assertEqual(false, to_bool(0)),
+     ?_assertEqual(true, to_bool(-1))
+    ].
+
+fetch_with_default_test_() ->
+    I = [{1, 2, 3}, {-1, -2, -3}],
+    Vars = lists:usort(lists:flatten([[abs(T1),abs(T2),abs(T3)] || {T1,T2,T3} <- I])),
+    NrOfVars = length(Vars),
+    Bools = [to_bool(bit(B, 0)) || B <- lists:seq(0, NrOfVars-1)],
+    Assignment = orddict:from_list(lists:zip(Vars, Bools)),
+    [
+     ?_assertEqual(false, fetch_with_default(1, Assignment, true)),
+     ?_assertEqual(false, fetch_with_default(2, Assignment, true)),
+     ?_assertEqual(false, fetch_with_default(3, Assignment, true)),
+     ?_assertEqual(true, fetch_with_default(4, Assignment, true))
+    ].
+
+pad_assignment_test_() ->
+    I = [{1, 2, 3}, {-1, -2, -3}],
+    Vars = lists:usort(lists:flatten([[abs(T1),abs(T2),abs(T3)] || {T1,T2,T3} <- I])),
+    NrOfVars = length(Vars),
+    Bools = [to_bool(bit(B, 0)) || B <- lists:seq(0, NrOfVars-1)],
+    Assignment = orddict:from_list(lists:zip(Vars, Bools)),
+    Max = lists:max(Vars),
+    [
+     ?_assertEqual([false, false, false], pad_assignment(Assignment, Max)),
+     ?_assertEqual([false, false, false, true], pad_assignment(Assignment, Max+1))
+    ].
+
+truth_value_test_() ->
+    I = [{1, 2, 3}, {-1, -2, -3}],
+    Vars = lists:usort(lists:flatten([[abs(T1),abs(T2),abs(T3)] || {T1,T2,T3} <- I])),
+    NrOfVars = length(Vars),
+    Bools = [to_bool(bit(B, 0)) || B <- lists:seq(0, NrOfVars-1)],
+    Assignment = orddict:from_list(lists:zip(Vars, Bools)),
+    [
+     ?_assertEqual(false, truth_value(1, Assignment)),
+     ?_assertEqual(false, truth_value(2, Assignment)),
+     ?_assertEqual(false, truth_value(3, Assignment))
+    ].
+
+check_clause_test_() ->
+    I = [{1, 2, 3}, {-1, -2, -3}],
+    Vars = lists:usort(lists:flatten([[abs(T1),abs(T2),abs(T3)] || {T1,T2,T3} <- I])),
+    NrOfVars = length(Vars),
+    Bools = [to_bool(bit(B, 0)) || B <- lists:seq(0, NrOfVars-1)],
+    Assignment = orddict:from_list(lists:zip(Vars, Bools)),
+    [
+     ?_assertEqual(false, check_clause({1, 2, 3}, Assignment))
+    ].
